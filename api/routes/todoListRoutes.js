f4a514de83910ce90212be44e5b279fc87092a64
kamomo'♥ แก้มโอโม่ <kamomo' >
2017-05-21 17:18:52 +0700

    update

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
 
 
   // todoList Routes
-  app.route('/tasks')
+  app.route('/lists')
     .get(todoList.list_all_tasks)
     .post(todoList.create_a_task);
 
 
-  app.route('/tasks/:Id')
+  app.route('/lists/:Id')
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
